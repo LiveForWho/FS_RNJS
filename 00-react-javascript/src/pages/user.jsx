69cd359248c2b456ed79c6dade5cdcd9b4e8e4fb
Huynh Tai <tai0898235536@gmail.com>
2025-05-10 08:34:21 +0700

    9

--- 00-react-javascript/src/pages/user.jsx
+++ 00-react-javascript/src/pages/user.jsx
@@ -1,7 +1,43 @@
-import React from "react";
+import { Table } from "antd";
+import { useEffect, useState } from "react";
+import { getUserApi } from "../util/api";
 
 const UserPage = () => {
-  return <div>UserPage</div>;
+  const [dataSource, setDataSource] = useState([]);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      const res = await getUserApi();
+      if (res) {
+        setDataSource(res);
+      }
+    };
+    fetchUser();
+  }, []);
+
+  const columns = [
+    {
+      title: "Id",
+      dataIndex: "_id",
+    },
+    {
+      title: "Email",
+      dataIndex: "email",
+    },
+    {
+      title: "Name",
+      dataIndex: "name",
+    },
+    {
+      title: "Role",
+      dataIndex: "role",
+    },
+  ];
+  return (
+    <div style={{ padding: 30 }}>
+      <Table dataSource={dataSource} columns={columns} rowKey={"_id"} />
+    </div>
+  );
 };
 
 export default UserPage;
