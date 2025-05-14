46cf27b21d23c057580d40712e78dcd0cae5b44e
Huynh Tai <tai0898235536@gmail.com>
2025-05-14 13:42:50 +0700

    11

---
+++ 01-nodejs-javascript/src/middleware/auth.js
@@ -0,0 +1,30 @@
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
+
+const auth = (req, res, next) => {
+  const whiteList = ["/", "/register", "/login"];
+  if (whiteList.find((item) => "/v1/api" + item === req.originalUrl)) {
+    next();
+  } else {
+    if (req.headers && req.headers.authorization) {
+      const token = req.headers.authorization.split(" ")[1];
+      //verify
+      try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        console.log("check token: ", decoded);
+      } catch (error) {
+        return res.status(401).json({
+          message: " token bị hết hạn hoặc không hợp lệ",
+        });
+      }
+
+      next();
+    } else {
+      return res.status(401).json({
+        message: "Bạn chưa truyền access token ở header hoặt token bị hết hạn",
+      });
+    }
+  }
+};
+
+module.exports = auth;
